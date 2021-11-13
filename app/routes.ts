3788b8bd579c58f11195f7cfbd8d8ae4037ef602
varshita178 <83539234+varshita178@users.noreply.github.com>
2021-11-13 13:14:31 +0530

    Delete app directory

--- app/routes.ts
+++
@@ -1,5 +0,0 @@
-import { Routes } from '@angular/router';
-import { WeatherComponent } from './weather/weather.component';
-import { Component } from '@angular/core';
-
-export const allAppRoutes: Routes = [{ path: '', component: WeatherComponent }];
